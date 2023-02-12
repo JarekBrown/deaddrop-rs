62ddc09b5bb6be4cb318c94af29de6dc3eaa941e
Jarek Brown <31749343+JarekBrown@users.noreply.github.com>
2023-02-12 10:50:20 -0700

    logging logic changed

--- src/logging.rs
+++ src/logging.rs
@@ -1,42 +1,27 @@
-use log::{error, info, warn, debug};
 use log4rs;
 
 /// ## Event logger
 ///
-/// Used to log the following events:
-/// - sending/reading a message to user that exists
-/// - creation of new user
-/// - reading message with the wrong password
-/// - reading message for a user that does not exist
-/// - sending message to user that does not exist
-/// - other important events that should be noted
+/// Initializes the log4rs logging environment, configured by log4rs.yaml file
 ///
-/// # Arguments
-///
-/// * `category` - either "info", "warn", or "error"
-/// * `command` - holds the logging message (and relevant context)
-///
-/// Returns Result<(), &str>
+/// Uses macros provided by log crate
 ///
 /// # Examples
 ///
 /// ```
 /// use deaddrop_rs::logging::log_event;
 ///
-/// log_event("info", "something happened");
-/// 
-/// log_event("error", "there was a problem here");
-/// 
-/// log_event("warn", "this could be an issue");
+/// info!("something happend");
+///
+/// error!("there was a problem here");
+///
+/// warn!("there could be an issue here");
 /// ```
-pub fn log_event(category: &str, event: String) {   
-    log4rs::init_file("log4rs.yaml", Default::default()).unwrap_or_else(|error| 
-        println!("Error encountered when initializing logger: {}", error) );
-
-    match category {
-        "info" => info!("{event}"),
-        "warn" => warn!("{event}"),
-        "error" => error!("{event}"),
-        _ => debug!("invalid category entered when trying to log: {category}, {event}")
-    }
+pub fn log_init() {
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap_or_else(|error| {
+        println!(
+            "ERROR: problem encountered when initializing logger -- {}",
+            error
+        )
+    });
 }
