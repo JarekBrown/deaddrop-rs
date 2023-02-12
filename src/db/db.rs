ecf55c6d560e5f568a64ae60853f57ff4c218663
Jarek Brown <31749343+JarekBrown@users.noreply.github.com>
2023-02-12 10:51:55 -0700

    fixed table creation issue

--- src/db/db.rs
+++ src/db/db.rs
@@ -1,6 +1,6 @@
+use log::info;
 use rusqlite::{Connection};
-use std::{fs, path::Path};
-use crate::logging::log_event;
+use std::{fs, path::Path, env};
 
 pub fn connect() -> Connection {
     let mut must_initialize_db = false;
@@ -11,13 +11,18 @@ pub fn connect() -> Connection {
     let connection = Connection::open("dd.db").unwrap();
     
     if must_initialize_db {
+        // in some cases, wrong line endings for an OS can cause only the first table to be created
+        let line_ending = match env::consts::OS {
+            "windows" => ";\r\n\r\n",
+            _ => ";\n"
+        };
         let query = fs::read_to_string("init.sql").expect("initial schema does not exist");
-        let commands = query.split(";\n");
+        let commands = query.split(line_ending);
 
         for command in commands {
             connection.execute(command, ()).unwrap();
         }
-        log_event("info", format!("database created"));
+        info!("database created");
     }
     
     return connection;
