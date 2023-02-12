f2be05a679b98de44fbf6cc2171eac015be9996f
Jarek Brown <31749343+JarekBrown@users.noreply.github.com>
2023-02-12 10:51:17 -0700

    fixed duplicate user issue

--- src/new.rs
+++ src/new.rs
@@ -1,4 +1,5 @@
-use crate::{session, db::users, logging::log_event};
+use crate::{db::users, session};
+use log::{error, info, warn};
 use std::io::{self, BufRead};
 
 pub fn new_user(user: String) {
@@ -8,12 +9,12 @@ pub fn new_user(user: String) {
     };
 
     if !users::no_users() && !user_exists {
-        log_event("error", format!("(new) user not found: {}", {user}));
+        error!("user not found: {}", user);
         panic!("User not recognized");
     }
 
     if !session::authenticate(user.clone()).expect("Unable to authenticate user") {
-        log_event("warn", format!("(new) unable to authenticate: {}", user));
+        warn!("unable to authenticate user: {}", user);
         panic!("Unable to authenticate user");
     }
 
@@ -21,11 +22,29 @@ pub fn new_user(user: String) {
     let new_user = get_new_username();
     let new_pass_hash = session::get_password();
 
-    users::set_user_pass_hash(new_user, new_pass_hash);
+    users::set_user_pass_hash(new_user.clone(), new_pass_hash);
 
-    log_event("info", format!("user created: {}", user));
+    info!("user created by {}: {}", user, new_user);
 }
 
 fn get_new_username() -> String {
-    io::stdin().lock().lines().next().expect("there was no next line").expect("the line could not be read")
+    loop {
+        let input = io::stdin()
+            .lock()
+            .lines()
+            .next()
+            .expect("there was no next line")
+            .expect("the line could not be read");
+
+        let user_exists = match users::get_user(input.clone()) {
+            Some(_) => true,
+            None => false,
+        };
+
+        if user_exists {
+            println!("Already taken, please try again:");
+        } else {
+            break input
+        }
+    }
 }
